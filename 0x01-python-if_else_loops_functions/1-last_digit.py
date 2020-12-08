3257aee8faab9c70accc2180b93551d911a464f5
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-12-08 20:02:02 +0000

    added py script that finds the last digit of a number and format prints it

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+# YOUR CODE HERE
+if number > 9:
+    new = number % 10
+elif number < -9:
+    new = number * -1 % 10
+string = "last digit of " + str(number) + " is " + str(new)
+if new > 5:
+    print("{} and is greater than 5".format(string))
+elif new < 6 and new != 0:
+    print("{} and is less than 6 and not 0".format(string))
+else:
+    print("{} and is 0".format(string))
