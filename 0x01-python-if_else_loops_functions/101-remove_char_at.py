1d3d39ddb8e2a8cbba36602d959bfb0279ac0e6f
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-12-09 03:04:36 +0000

    needed to add some base checks to get correct output using the C index concpet

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    str_cpy = str[0:n] + str[n + 1:]
-    return str_cpy
+    if n > -1:
+        str_cpy = str[0:n] + str[n + 1:]
+        return str_cpy
+    else:
+        return str
