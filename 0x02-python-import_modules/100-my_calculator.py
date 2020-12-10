5c46dd7aac69c3b80eb52653479a2b9b10ed054a
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-12-10 20:21:43 +0000

    forgot to add space in block comment

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#this program uses calculator_1 module to interpret input into a calculator
+# this program uses calculator_1 module to interpret input into a calculator
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
