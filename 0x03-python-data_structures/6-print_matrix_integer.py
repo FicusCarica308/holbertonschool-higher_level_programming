60ed1d626520669107221b32a97d74fe8519d710
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-12-15 20:43:59 +0000

    formatted integer using [:d]

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for inner in matrix:
         for i in range(0, len(inner)):
-            print("{}".format(inner[i]), end="")
+            print("{:d}".format(inner[i]), end="")
             if i < len(inner) - 1:
                 print(" ", end="")
         print()
