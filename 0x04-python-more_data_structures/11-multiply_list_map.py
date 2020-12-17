d5fffe0b17ed902a6db19849d5d163d5166eaa36
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-12-17 22:00:42 +0000

    added task 11 using map function

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    copy = my_list.copy()
-    copy[:] = map(lambda x: x*number, copy)
-    return copy
+    return list(map(lambda x: x*number, my_list.copy()))
