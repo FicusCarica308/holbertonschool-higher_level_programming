c137b221681d578fc5cba8b28d64fd8c985d6f7d
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-12-17 21:01:01 +0000

    trying new method

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,14 +2,5 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    num = 0
-    num_hold = 0
-    key_hold = ""
-    for key in a_dictionary:
-        num = a_dictionary.get(key, None)
-        if num == None:
-            num = 0
-        if num > num_hold:
-            key_hold = key
-        num_hold = a_dictionary.get(key_hold, None)
+    key_hold = max(a_dictionary, key=a_dictionary.get)
     return key_hold
