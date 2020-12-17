05def19721aa129cc15df85b2bd6d7df09a90941
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-12-17 18:21:02 +0000

    reverted changes

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list[:]
-    idx = new_list.index(search)
-    new_list[idx] = replace
+    for i in range(0, len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
     return new_list
