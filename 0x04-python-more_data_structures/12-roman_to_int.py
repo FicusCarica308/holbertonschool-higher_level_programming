04d5974fd04cf5c423e60e3fad56d89149fb29ac
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-12-17 22:48:19 +0000

    added complete definiton 12-romand_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    final = 0
+    numerals = {'MMM': 3000, 'MM': 2000, 'M': 1000, 'CM': 900, 'DCCC': 800,
+                'DCC': 700, 'DC': 600, 'D': 500, 'CD': 400, 'CCC': 300,
+                'CC': 200, 'C': 100, 'XC': 90, 'LXXX': 80, 'LXX': 70,
+                'LX': 60, 'L': 50, 'XL': 40, 'XXX': 30, 'XX': 20, 'X': 10,
+                'IX': 9, 'VIII': 8, 'VII': 7, 'VI': 6, 'V': 5, 'IV': 4,
+                'III': 3, 'II': 2, 'I': 1
+                }
+    for key in numerals:
+        if key in roman_string:
+            roman_string = roman_string.replace(key, "")
+            final += numerals.get(key)
+    return final
