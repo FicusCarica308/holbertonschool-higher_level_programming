0b490e2924c326034106aba582498c8122d6ceb0
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-12-17 20:43:46 +0000

    added check to see if individual key is None or not

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -6,8 +6,10 @@ def best_score(a_dictionary):
     num_hold = 0
     key_hold = ""
     for key in a_dictionary:
-        num = a_dictionary.get(key)
+        num = a_dictionary.get(key, None)
+        if num == None:
+            num = 0
         if num > num_hold:
             key_hold = key
-        num_hold = a_dictionary.get(key_hold)
+        num_hold = a_dictionary.get(key_hold, None)
     return key_hold
