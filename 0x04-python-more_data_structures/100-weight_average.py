468ed5c3dbfbed5beae88e3f56f25fc3164744e7
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-12-18 01:51:54 +0000

    added advanced task

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if len(my_list) == 0
+        return 0
+    grades = list()
+    weights = list()
+    sum = 0
+    div = 0
+    for i in range(0, 4):
+        for j in [0]:
+            grades.append(my_list[i][j])
+        for j in [1]:
+            weights.append(my_list[i][j])
+    for i in range(0, 4):
+        sum = sum + (weights[i] * grades[i])
+        div = div + weights[i]
+    return sum / div
