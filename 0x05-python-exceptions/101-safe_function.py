36d07ba1d46ce9a36c784785010787ef70cb0c08
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-01-05 03:25:52 +0000

    added a function to call a two integer function safely

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,6 +5,6 @@ import sys
 def safe_function(fct, *args):
     try:
         return fct(args[0], args[1])
-    except Exception as error:
+    except (TypeError, ValueError, IndexError, ZeroDivisionError) as error:
         sys.stderr.write("Exception: {}\n".format(error))
         return None
