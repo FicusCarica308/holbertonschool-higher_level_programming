0cb04e6099ae038307416b64bfee94066b3d3e31
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-01-06 02:32:38 +0000

    added comments and pep8 format

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,8 +3,9 @@
 
 
 class Square:
+    """Square class with a size private field (__size)"""
     def __init__(self, size=0):
-        """Function that initializes rpivate attribute
+        """Function that initializes pivate attribute
         containing the size of the square
         """
         self.__size = size
