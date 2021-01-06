2b30c07b51f1cf7488e101248b2f50847e22706d
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-01-06 18:34:28 +0000

    added one last check for new_node method

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This is a class that mimicks a C programs singly linked list"""
 
+
 class Node:
     """private field initialization"""
     def __init__(self, data, next_node=None):
@@ -10,6 +11,7 @@ class Node:
         self.data = data
         self.next_node = next_node
 # ----------------------__data atribute methods
+
     @property
     def data(self):
         """getter property for private attribute __data"""
@@ -23,6 +25,7 @@ class Node:
         else:
             self.__data = value
 # ----------------------__next_node attribute methods
+
     @property
     def next_node(self):
         """getter property for private attribute __next_node"""
@@ -31,10 +34,10 @@ class Node:
     @next_node.setter
     def next_node(self, value):
         """setter to set __next_node private attribute"""
-        # if type(next_node) is not Node:
-         #   raise TypeError("next_node must be a Node object")
-        # else:
-        self.__next_node = value
+        if isinstance(value, Node) is not True and value is not None:
+            raise TypeError("next_node must be a Node object")
+        else:
+            self.__next_node = value
     # ------------------------------------------------------
 """Class called singly linkes list"""
 
@@ -70,7 +73,7 @@ class SinglyLinkedList:
         with a number on each line for the length of the linked list
         """
         output = ""
-        ptr = self.__head 
+        ptr = self.__head
         while ptr:
             if ptr.next_node is not None:
                 output += str(ptr.data) + "\n"
