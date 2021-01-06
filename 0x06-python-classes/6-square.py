36219052858c54929d003b41f32384d472397ff2
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-01-06 03:19:41 +0000

    changed around position exceptions checks

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -47,7 +47,7 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         elif len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif isinstance(value[0], int) is not True or isinstance(value[1], int) is not True:
+        elif type(value[0]) is not int or type(value[1]) is not int:
             raise TypeError("position must be a tuple of 2 positive integers")
         elif value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
