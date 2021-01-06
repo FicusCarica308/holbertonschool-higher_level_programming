3061639007dc8d33e86a7ecffabe141e61e6f382
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-01-06 19:36:54 +0000

    added

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -46,10 +46,10 @@ class Square:
     def __lt__(self, other):
         return (self.__size * self.__size) < (other.__size * other.__size)
     def __le__(self, other):
-        return (self.__size * self.__size) < (other.__size * other.__size)
+        return (self.__size * self.__size) <= (other.__size * other.__size)
     def __ne__(self, other):
-        return (self.__size * self.__size) < (other.__size * other.__size)
+        return (self.__size * self.__size) != (other.__size * other.__size)
     def __gt__(self, other):
-        return (self.__size * self.__size) < (other.__size * other.__size)
+        return (self.__size * self.__size) > (other.__size * other.__size)
     def __ge__(self, other):
-        return (self.__size * self.__size) < (other.__size * other.__size)
+        return (self.__size * self.__size) >= (other.__size * other.__size)
