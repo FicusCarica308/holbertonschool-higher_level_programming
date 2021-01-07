d77ab592e7c8ee0a4a8ea3c076fae00a4a30aa0f
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-01-07 03:30:26 +0000

    fixed bytecode errrors

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
-import dis
-"""Place"""
+import math
+"""This is decoded from ByteCode"""
 
 
 class MagicClass:
-    """PLace"""
-    def __init__(self):
-        """place"""
-        self.__radius
+    """MAGIC CLASS"""
+    def __init__(self, radius):
+        """doc string 1"""
+        self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
-            raise TypeError('radius must be a number')
+            raise TypeError("radius must be a number")
         self.__radius = radius
-dis.dis(MagicClass)
+
+    def area(self, radius):
+        """doc string 2"""
+        return self.__radius ** 2 * math.pi
+
+    def circumference(self, radius):
+        """doc string 3"""
+        return 2 * math.pi * self.__radius
