0c87850544c01d425339a4e8a78f18a3034f1c4e
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-01-08 05:17:33 +0000

    added complete script 0-add_integer.py along with README and tests directory

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+0-add_integer.py:
+    This Module contains a function method that adds two
+    given integers together.
+"""
+
+
+def add_integer(a, b=98):
+    """
+    add_integer:
+        adds two integers/floats
+    Paramaters:
+    a (int or float): intger input by user
+    b (int or float): integer input by user
+
+    Returns:
+        returns the addition of parameters a and b (a + b)
+
+    Examples:
+        Integers:
+        >>> add_integer(2, 2)
+        4
+
+        Floats: (rounds down since floats are casted to ints)
+        >>> add_integer(5.7, 5.4)
+        10
+    """
+    if isinstance(a, (int, float)) is not True:
+        raise TypeError("a must be an integer")
+    elif isinstance(b, (int, float)) is not True:
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return a + b
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,7 @@
+Name: MANUEL FIGUEROA/
+Date: 1 - 8 - 2021/
+Project: 0x07-python-test_driven_development/
+Description: In this directory there is a sub directory called tests/
+that contains doctests .txt files for each .py script/
+each .py script is commented and all edgecases are though of prechecker/
+to help us learn how to code using test_driven_development
