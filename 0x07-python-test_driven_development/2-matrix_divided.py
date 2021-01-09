9acdbd370ba74e00e939e6c01fd962b57a6c566e
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-01-09 04:15:17 +0000

    added matrix diveded code and test.txt file

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+2-add_integer.py:
+    This Module contains a function method that will divide a
+    given matrix of integers/floats by a given divisor
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+    matrix_divided:
+        divides a matrix of float/int by a given divisor
+    Paramaters:
+    matrix(list)needs to be integers: matrix of integers input
+    div(int): our divisors
+
+    Returns:
+        returns a new matrrx with the result of each division
+        rounded to the second decimal place
+    """
+    if isinstance(matrix, list) is not True or matrix[0] is None:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    for x in matrix:
+        if type(x) is not list:
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    elif isinstance(div, (int, float)) is not True:
+        raise TypeError("div must be a number")
+
+    new_matrix = [row[:] for row in matrix]
+    row_lgt = len(matrix[0])
+
+    for row in range(len(matrix)):
+        for column in range(len(matrix[row])):
+            if isinstance(matrix[row][column], (int, float)) is not True:
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            new_matrix[row][column] = round(matrix[row][column] / div, 2)
+        if column != row_lgt - 1:
+                raise TypeError("Each row of the matrix must have the same size")
+
+    return new_matrix
