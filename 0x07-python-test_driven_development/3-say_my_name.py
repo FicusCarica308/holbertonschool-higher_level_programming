f829a09edb216f47b1608889715d40c5e2a8e567
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-01-09 04:36:09 +0000

    added say my name script with doctest file

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+3-say_my_name.py:
+    This Module contains a function method that prints
+    out a given name (first, last)
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    say_my_name:
+        prints first_name and last_name in stdout
+    Paramaters:
+        first_name(string) = first name of user
+        last_name(string) = last name of user
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
