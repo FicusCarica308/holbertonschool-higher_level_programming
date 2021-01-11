1d1c5b47ead7ea656f328abf4c6b45e0233876ce
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-01-11 21:37:49 +0000

    added indentation method

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""module contains a def to add new lines to a given text"""
+
+
+def text_indentation(text):
+    """The def that adds the new lines"""
+    new_string = ""
+    print_string = ""
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    for i in range(len(text)):
+        new_string += text[i]
+        if text[i] == '.' or text[i] == '?' or text[i] == ':':
+            new_string += "\n\n"
+    for i in range(len(new_string)):
+        if new_string[i] == ' ' and new_string[i - 1] == '\n':
+            continue
+        print_string += new_string[i]
+    print(print_string)
