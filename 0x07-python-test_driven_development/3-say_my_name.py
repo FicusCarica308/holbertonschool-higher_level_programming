a417bd92005a65c87ece3a51077f9c48e0a7738e
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-01-11 21:06:52 +0000

    removed comments

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -15,9 +15,7 @@ def say_my_name(first_name, last_name=""):
         last_name(string) = last name of user
     """
     if type(first_name) is not str:
-        """why"""
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
-        """why"""
         raise TypeError("last_name must be a string")
     print("My name is {:s} {:s}".format(first_name, last_name))
