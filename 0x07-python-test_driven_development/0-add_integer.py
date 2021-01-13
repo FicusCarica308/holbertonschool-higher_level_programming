62bceaf370c1542eebf4960f42a7d6ad6dec0bdc
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-01-13 18:39:16 +0000

    added Nan check

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -26,8 +26,10 @@ def add_integer(a, b=98):
         >>> add_integer(5.7, 5.4)
         10
     """
-    if isinstance(a, (int, float)) is not True:
+    Not_a_num = a != a
+    Not_a_num2 = b != b
+    if isinstance(a, (int, float)) is not True or Not_a_num is not False:
         raise TypeError("a must be an integer")
-    elif isinstance(b, (int, float)) is not True:
+    elif isinstance(b, (int, float)) is not True or Not_a_num2 is not False:
         raise TypeError("b must be an integer")
     return int(a) + int(b)
