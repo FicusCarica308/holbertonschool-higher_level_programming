292a1675f244f5b6dbef0f88df07f17308070298
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-01-14 00:53:19 +0000

    added pep8

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -18,11 +18,12 @@ def matrix_divided(matrix=None, div=None):
         returns a new matrrx with the result of each division
         rounded to the second decimal place
     """
+    matrix_error = "matrix must be a matrix (list of lists) of integers/floats"
     if isinstance(matrix, list) is not True or matrix[0] is None:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(matrix_error)
     for x in matrix:
         if type(x) is not list:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError(matrix_error)
     if div == 0:
         raise ZeroDivisionError("division by zero")
     elif isinstance(div, (int, float)) is not True:
@@ -34,7 +35,7 @@ def matrix_divided(matrix=None, div=None):
     for row in range(len(matrix)):
         for column in range(len(matrix[row])):
             if isinstance(matrix[row][column], (int, float)) is not True:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(matrix_error)
             new_matrix[row][column] = round(matrix[row][column] / div, 2)
         if column != row_lgt - 1:
                 raise TypeError("Each row of the matrix must have the same size")
