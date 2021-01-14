192bc2b209b29b51ae70b3f20e3b090894b527e6
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-01-14 00:57:04 +0000

    added no argument check'

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -6,7 +6,7 @@
 """
 
 
-def say_my_name(first_name=None, last_name=None):
+def say_my_name(first_name=None, last_name=""):
     """
     say_my_name:
         prints first_name and last_name in stdout
