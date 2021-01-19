5aa2fa30f9afd4b0eea10ba7c49a2879a79a94e8
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-01-19 23:16:39 +0000

    removed lambda from code

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -9,7 +9,6 @@ def add_attribute(obj, name, value):
     """
     for i in dir(obj):
         if i == "__dict__":
-            obj.name = lambda: None
             setattr(obj, name, value)
             return
     raise TypeError("can't add new attribute")
