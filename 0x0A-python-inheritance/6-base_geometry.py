f951438bf40fee85385202ace8a8b17b388bcecf
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-01-19 18:55:50 +0000

    added def to raise exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module with a single class"""
+
+
+class BaseGeometry:
+    """empy class"""
+    def area(self):
+        """raises a excpetion from BaseException"""
+        raise Exception("area() is not implemented")
