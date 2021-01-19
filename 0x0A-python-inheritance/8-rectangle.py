f1d9140ca3c49050a8103d316dc99898fdb5bb15
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-01-19 19:22:39 +0000

    added class that inherits BaseGeometry from 7-base_geometry.py

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module contains a class the inherits BaseGerometry from file 7"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """class that initilizes value using BaseGeomatry"""
+    def __init__(self, width, height):
+        """
+        uses integer_validator from superclass to check given values
+        and assigns them to private fields in rectangle class
+        """
+        super().integer_validator("height", height)
+        super().integer_validator("width", width)
+        self.__height = height
+        self.__width = width
