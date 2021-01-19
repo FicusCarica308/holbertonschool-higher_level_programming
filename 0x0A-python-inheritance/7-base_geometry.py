a272487facdb71b4ac4732c42f6406a7e32853b6
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-01-19 19:01:36 +0000

    added a new def to BaseGeromatry called integer_validator

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module with a single class"""
+
+
+class BaseGeometry:
+    """empy class"""
+    def area(self):
+        """raises a excpetion from BaseException"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """tests if given value is a integer and greater than 0"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise TypeError("{} must be greater than 0".format(name))
