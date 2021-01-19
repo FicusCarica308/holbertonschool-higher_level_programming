088b03e65eeca663d49375cdce4962b0fbbe39cb
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-01-19 23:30:00 +0000

    removed superclass call

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,10 +6,6 @@ class MyList(list):
     """
     Class that creates a different list object using the inherited list object
     """
-    def __init__(self):
-        """will init our class with our superclass intit function"""
-        super().__init__()
-
     def print_sorted(self):
         """prints a sorted list instead of a normal list"""
         copy_list = list(self)
