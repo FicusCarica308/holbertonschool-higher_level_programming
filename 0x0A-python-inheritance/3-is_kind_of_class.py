d6b2e2cc9c8feb59a7bcd4e244bbae36f73f6eda
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-01-19 15:06:05 +0000

    added function to tell if a object is a instance of a given class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" contains one function to test if objects are from a certain class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ Tests if object given is class of object is the same"""
+    return isinstance(obj, a_class)
