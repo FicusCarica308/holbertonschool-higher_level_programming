899d2e18f0f5e29317a1264892d78a9683a553c4
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-01-19 14:11:20 +0000

    added comments to function and module

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""Module with a single function to lookup functions in a class"""
+
+
 def lookup(obj):
+    """function to print attributes of a class using dir()"""
     return dir(obj)
