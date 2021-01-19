32810a954684fad59e69932a9dbca048f94dd416
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-01-19 20:06:44 +0000

    added __str__ overide to square

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module contains a class the inherits BaseGerometry from file 7"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        """will init square with the init of rectangle"""
+        super().integer_validator("size", size)
+        self.__size = size
+        Rectangle.__init__(self, size, size)
+
+    def __str__(self):
+        """returns a string of a square description"""
+        return "[square] {0}/{0}".format(self.__size)
