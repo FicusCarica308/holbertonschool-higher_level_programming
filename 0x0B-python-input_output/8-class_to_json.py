d185fa7aeefb4eaed16daff96b0eace2d82a03d8
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-01-21 01:25:13 +0000

    added non import version of code

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """Module that has a function using json encoding"""
-import json
 
 
 def class_to_json(obj):
     """converts a class to json"""
-    other = json.dumps(obj.__dict__)
-    return json.loads(other)
+    return obj.__dict__
