9f392eb6c5e3520f12ca5679633ade63e2a6adbf
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-01-26 22:28:32 +0000

    added a commented and pep8 fixes

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -3,9 +3,9 @@
 Test cases for rectangle.py
 """
 import unittest
-from io import StringIO 
-from unittest import TestCase 
-from unittest.mock import patch 
+from io import StringIO
+from unittest import TestCase
+from unittest.mock import patch
 from models.base import Base
 from models.rectangle import Rectangle
 
@@ -16,17 +16,20 @@ class TestBaseClass(unittest.TestCase):
     test_id_input - tests if one id is given to the Base class
     """
     def setUp(self):
+        """ init's nb_objects """
         Base._Base__nb_objects = 0
 
     def test_getters(self):
+        """ Tests getters """
         rect = Rectangle(10, 2, 3, 4, 12)
         self.assertEqual(rect.width, 10)
         self.assertEqual(rect.height, 2)
         self.assertEqual(rect.x, 3)
         self.assertEqual(rect.y, 4)
         self.assertEqual(rect.id, 12)
-    
+
     def test_setters(self):
+        """ Tests setters """
         rect = Rectangle(10, 2, 3, 4, 12)
         # ---Width ---
         rect.width = 24
@@ -43,9 +46,9 @@ class TestBaseClass(unittest.TestCase):
         # ---ID ---
         rect.id = 127
         self.assertEqual(rect.id, 127)
-        
 
     def test_value_errors(self):
+        """ Tests value errors """
         rect = Rectangle(10, 2, 3, 4, 12)
         with self.assertRaisesRegex(ValueError, "width must be > 0"):
             rect.width = -10
@@ -61,6 +64,7 @@ class TestBaseClass(unittest.TestCase):
             rect.y = -30
 
     def test_type_errors(self):
+        """ Tests Type errors """
         rect = Rectangle(10, 2, 3, 4, 12)
         with self.assertRaisesRegex(TypeError, "width must be an integer"):
             rect.width = dict()
@@ -81,37 +85,42 @@ class TestBaseClass(unittest.TestCase):
             rect.y = None
 
     def test_area(self):
+        """ Tests area method """
         rect = Rectangle(10, 2, 3, 4, 12)
         self.assertEqual(rect.area(), 20)
         rect = Rectangle(30, 10, 3, 4, 12)
         self.assertEqual(rect.area(), 300)
 
     def test_print(self):
+        """ Tests print method """
         r1 = Rectangle(4, 6, 3, 4, 12)
         expected = "####\n####\n####\n####\n####\n####\n"
-        with patch('sys.stdout', new = StringIO()) as fake_out:
+        with patch('sys.stdout', new=StringIO()) as fake_out:
             r1 = Rectangle(4, 6)
             r1.display()
         self.assertEqual(fake_out.getvalue(), expected)
 
         expected = "\n ####\n ####\n ####\n ####\n ####\n ####\n"
-        with patch('sys.stdout', new = StringIO()) as fake_out:
+        with patch('sys.stdout', new=StringIO()) as fake_out:
             r1 = Rectangle(4, 6, 1, 1)
             r1.display()
         self.assertEqual(fake_out.getvalue(), expected)
 
     def test_str_out(self):
+        """ Tests str print() method """
         expected_out = "[Rectangle] (12) 2/1 - 4/6"
         rect = Rectangle(4, 6, 2, 1, 12)
         self.assertEqual(str(rect), "[Rectangle] (12) 2/1 - 4/6")
 
     def test_to_dict(self):
+        """ Tests todict() method """
         r1 = Rectangle(10, 2, 1, 9)
         r1_dictionary = r1.to_dictionary()
         test_dict = {'x': 1, 'y': 9, 'id': 1, 'height': 2, 'width': 10}
         self.assertDictEqual(r1_dictionary, test_dict)
 
     def test_update(self):
+        """ Tests update method """
         # -----ARGS--------
         r1 = Rectangle(10, 10, 10, 10)
         self.assertEqual(str(r1), "[Rectangle] (1) 10/10 - 10/10")
