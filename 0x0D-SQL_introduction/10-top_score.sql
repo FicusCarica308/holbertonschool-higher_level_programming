caa4fac01baf1b61c8e916f5c195b4f719f4e2c6
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-03-01 14:14:06 +0000

    swapped name and score positions cause output was wrong

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 --  lists name and scroe rows of the table second_table from the database hbtn_0c_0 with score being in descending order
-SELECT name, score FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
