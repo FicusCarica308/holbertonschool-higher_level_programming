9fb0694fd2ac3b50b23da80e306e3828c73c5d79
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-03-01 14:34:14 +0000

    added greater than threshold

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,2 @@
+--  lists name and scroe rows of the table second_table from the database hbtn_0c_0 with score being in descending order
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
