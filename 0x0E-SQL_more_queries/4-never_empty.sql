75a21d11aeb59996521831cb5cc5a875f0637440
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-03-01 17:49:28 +0000

    added SQL to create table with defualt value

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,5 @@
+-- a script that creates the table id_not_null on your MySQL server.
+-- id has defuslt value 1 and cannot be NULL
 CREATE TABLE IF NOT EXISTS id_not_null (
     id INT NOT NULL DEFAULT 1,
     name VARCHAR(256)
