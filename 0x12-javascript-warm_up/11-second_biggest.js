4310ccdcdf9e8b47d27e93a7175150e69c530fe2
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-03-23 02:09:22 +0000

    added sort command the make sure program works with greatest first

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 let first = 0;
 let second = 0;
+process.argv.sort();
 for (let iter = 2; iter < process.argv.length; iter++) {
   const num = Math.floor(process.argv[iter]);
   if (num > first) {
