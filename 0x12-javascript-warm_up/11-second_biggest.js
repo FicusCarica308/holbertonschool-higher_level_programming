1dbe42e531d5393d6cf092b283c2613187228a94
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-03-23 01:46:46 +0000

    removed prev commit changes

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
 let first = 0;
 let second = 0;
-if (process.argv.length <= 3) {
-  console.log('0');
-} else {
 for (let iter = 2; iter < process.argv.length; iter++) {
-    const num = Math.floor(process.argv[iter]);
-    if (num > first) {
-      second = first;
-      first = num;
-    }
+  const num = Math.floor(process.argv[iter]);
+  if (num > first) {
+    second = first;
+    first = num;
   }
-  console.log(second);
 }
+console.log(second);
