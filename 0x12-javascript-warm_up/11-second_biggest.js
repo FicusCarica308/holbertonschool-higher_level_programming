ed01d50b62e7e09059a4169afbd2558036ed5f35
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-03-23 01:31:59 +0000

    added task 11 .js file

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+let first = 0;
+let second = 0;
+for (let iter = 2; iter < process.argv.length; iter++) {
+  const num = Number(process.argv[iter]);
+  if (num > first) {
+    second = first;
+    first = num;
+  }
+}
+console.log(second);
