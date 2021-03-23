96c9b5a461607edeeaf5b4c4c51d87aa2d6eecc6
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-03-23 00:52:55 +0000

    added script that prints <c is fun> a given amount of times

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const checkArg = Number(process.argv[2]);
+let i = 0;
+if (Object.is(NaN, checkArg) === true) {
+  console.log('Missing number of occurrences');
+} else {
+    let num = Math.floor(process.argv[2])
+    for (i = 0; i < num; i++) {
+        console.log('C is fun');
+      }
+  }
+  
