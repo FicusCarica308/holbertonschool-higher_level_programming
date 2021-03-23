b1f7ef1b45613b16c7455f2ba9b0a24a3a2c312c
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-03-23 01:50:39 +0000

    added change to start of for loop

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 let first = 0;
 let second = 0;
-for (let iter = 3; iter < process.argv.length; iter++) {
+for (let iter = 2; iter < process.argv.length; iter++) {
   const num = Math.floor(process.argv[iter]);
   if (num > first) {
     second = first;
