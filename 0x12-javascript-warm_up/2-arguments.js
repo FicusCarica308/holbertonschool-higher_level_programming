477fbd5e45b3640e5c6b51cf92d5dacb216d5898
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-03-23 00:01:26 +0000

    needed to fix semistandard errors

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-let argc = process.argv.length;
+const argc = process.argv.length;
 if (argc <= 2) {
   console.log('No argument');
-} else if (argc === 3){
+} else if (argc === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
