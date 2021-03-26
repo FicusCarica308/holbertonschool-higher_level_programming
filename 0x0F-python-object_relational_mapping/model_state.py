e8cbd330ba56e08f6b803866528dcc10c71befce
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-03-26 06:14:51 +0000

    added state class model for task 6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Contains the class definition of the states database"""
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+
+class State(Base):
+    """Database Class State"""
+    __tablename__ = "states"
+    id = Column('id', Integer, primary_key=True, nullable=False)
+    name = Column('name', String(128), nullable=False)
