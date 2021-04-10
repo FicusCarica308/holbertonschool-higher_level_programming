fbe3803304bf249a79afa13e79236f1ada6c9eaa
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-04-10 03:55:38 +0000

    added advanced task

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,7 +2,7 @@
 """
 Please list 10 commits (from the most recent to oldest) of
 the repository “rails” by the user “rails”
-You must use the GitHub API, here is the documentation 
+You must use the GitHub API, here is the documentation
 https://developer.github.com/v3/repos/commits/
 Print all commits by: `<sha>: <author name>` (one by line)
 """
