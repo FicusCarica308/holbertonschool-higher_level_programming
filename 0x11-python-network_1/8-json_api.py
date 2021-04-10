dd8a38d70538ddaf242681fa5de5f32da12539ca
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-04-10 03:18:48 +0000

    reverted post move and removed exception parameter

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -3,9 +3,6 @@
 Description:
 a Python script that takes in a letter and sends a POST request to
 http://0.0.0.0:5000/search_user with the letter as a parameter.
-
-Checks for invalid json using try: except:
-with json.JSONDecodeError
 """
 
 
@@ -17,8 +14,8 @@ if __name__ == "__main__":
         param = {'q': ""}
     else:
         param = {'q': argv[1]}
+    response = requests.post("http://0.0.0.0:5000/search_user", data=param)
     try:
-        response = requests.post("http://0.0.0.0:5000/search_user", data=param)
         json = response.json()
         if not json:
             print("No result")
