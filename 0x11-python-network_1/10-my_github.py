49ebbe9c8061323019e0406188ad8231e09c9cf2
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-04-10 03:15:55 +0000

    pep8 error fix

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -10,5 +10,6 @@ if __name__ == "__main__":
     import requests
     from sys import argv
 
-    response = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
+    response = requests.get('https://api.github.com/user',
+                            auth=(argv[1], argv[2]))
     print(response.json().get('id'))
