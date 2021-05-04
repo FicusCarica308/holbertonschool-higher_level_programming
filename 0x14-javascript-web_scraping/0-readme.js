68c44908776d9acb7a91fce3e0725ac23c3e2288
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-05-04 23:09:30 +0000

    added task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const fs = require('fs');
+const process = require('process');
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    return console.error(err);
+  }
+  console.log(data);
+});
