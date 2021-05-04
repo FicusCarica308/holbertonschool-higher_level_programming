c7656ce1fe72d7d471cee37d8cbfb0d8f219527b
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-05-04 23:26:09 +0000

    added task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const request = require('request');
+const process = require('process');
+const options = {
+  url: process.argv[2],
+  method: 'GET'
+};
+request(options, (err, res, body) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log('code:', res && res.statusCode);
+});
