e78dcb7e88c92c0b091fe105517b177ca700e04a
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-05-04 23:55:35 +0000

    added semistandard fixes and variable error

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -9,14 +9,15 @@ request(options, (err, res, body) => {
   if (err) {
     return console.log(err);
   }
+  let numFilms = 0;
   const films = JSON.parse(body).results;
   for (let film = 0; film < films.length; film++) {
-    for (let chara = 0; chara < films[i].characters.length; chara++) {
+    for (let chara = 0; chara < films[film].characters.length; chara++) {
       if (films[film].characters[chara].split('/')[5] === '18') {
-        count++;
+        numFilms++;
         break;
       }
     }
   }
-  console.log(count);
+  console.log(numFilms);
 });
